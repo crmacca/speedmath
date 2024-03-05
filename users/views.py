f0b7b7cf5d7c00ad2a70322f8aea13b5ea62f4bb
crmacca <tozzleboy@gmail.com>
2024-03-05 14:52:55 +1100

    Inital Commit + Login Page, Styling and Message Setup

---
+++ users/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+
+def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+    return render(request, "users/user.html")
+    
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "users/login.html", {
+                'message': {
+                    "text": "Credentials combination does not match our records.",
+                    "type": "Error",
+                    "class": "errorBox"
+                }
+            })
+    return render(request, "users/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return render(request, "users/login.html", {
+        "message": {
+            "text": "Successfully Logged Out",
+            "type": "Success",
+            "class": "successBox"
+        }
+    })
